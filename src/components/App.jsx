62073df07180b5e9de0bc2489e2ab3243df4781c
Esteban Vasco <evasco.27@hotmail.com>
2022-05-12 16:16:17 -0500

    Primer Contenedor Funcionando

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,15 @@ completa para los snippets si es que no les resulta
 el imr ocupen el comando “rafce” para crear el componente.*/
 
 import React from 'react';
-import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
   return (
-    <h1>Hola mundo</h1>
+    <Layout>
+      <Login />
+    </Layout>
   )
 }
 
