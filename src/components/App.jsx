211d60df9e6dc70c27a01a529bb37c579619f866
Esteban Vasco <evasco.27@hotmail.com>
2022-05-12 15:34:54 -0500

    InitCommit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+/* //Les recomiendo esta extensión de react que es mas 
+completa para los snippets si es que no les resulta 
+el imr ocupen el comando “rafce” para crear el componente.*/
+
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+  return (
+    <h1>Hola mundo</h1>
+  )
+}
+
+export default App
