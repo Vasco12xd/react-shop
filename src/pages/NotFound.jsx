c4629c81ee69a146003c91e2094cd497797160b2
Esteban Vasco <evasco.27@hotmail.com>
2022-05-13 14:50:00 -0500

    Cambios de NotFound y react dom

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/NotFound.scss'
+
+const NotFound = () => {
+  return (
+    <div className='Container'>
+    <img src="https://i.pinimg.com/originals/3c/df/50/3cdf50bf2536bb0a51cb28e1a291e670.gif" alt="" />
+    </div>
+  )
+}
+
+export default NotFound
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,27 @@
 /* //Les recomiendo esta extensión de react que es mas 
 completa para los snippets si es que no les resulta 
 el imr ocupen el comando “rafce” para crear el componente.*/
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
+import Home from "../pages/Home";
+import NotFound from '../pages/NotFound';
 import RecoveryPassword from '../containers/recoveryPassword';
 import '../styles/global.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-          <Layout>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/RecoveryPassword' component={RecoveryPassword} />
-            <Route component={NotFound} />
-          </Layout>
-      </Switch>
-    </BrowserRouter>
+	<Layout>
+		<Routes>
+			<Route exact path="/" element={<Home />} />
+			<Route exact path="/login" element={<Login />} />			
+      <Route exact path="/recovery-password" element={<RecoveryPassword />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	</Layout>
+</BrowserRouter> 
   )
 }
 
