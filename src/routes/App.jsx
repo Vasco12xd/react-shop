ccbba5ece4e258ab3b688888ca0d268bcca642b7
Esteban Vasco <evasco.27@hotmail.com>
2022-05-13 15:34:00 -0500

    Paginas agregadas del proyecto frontdevelopment

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,10 +4,16 @@ el imr ocupen el comando “rafce” para crear el componente.*/
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
+import Login from '../pages/Login';
 import Home from "../pages/Home";
 import NotFound from '../pages/NotFound';
-import RecoveryPassword from '../containers/recoveryPassword';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+import RecoveryPassword from '../pages/recoveryPassword';
 import '../styles/global.css';
 
 const App = () => {
@@ -17,7 +23,14 @@ const App = () => {
 		<Routes>
 			<Route exact path="/" element={<Home />} />
 			<Route exact path="/login" element={<Login />} />			
-      <Route exact path="/recovery-password" element={<RecoveryPassword />} />
+      		<Route exact path="/recovery-password" element={<RecoveryPassword />} />
+			<Route exact path="/send-email" element={<SendEmail />} />
+			<Route exact path="/new-password" element={<NewPassword />} />
+			<Route exact path="/account" element={<MyAccount />} />
+			<Route exact path="/signup" element={<CreateAccount/>} />
+			<Route exact path="/checkout" element={<Checkout/>} />
+			<Route exact path="/orders" element={<Orders/>} />
+			
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	</Layout>
