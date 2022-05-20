082aea8bc30743feafa255816fde2c28fd921c17
Esteban Vasco <evasco.27@hotmail.com>
2022-05-20 14:08:52 -0500

    Revert "Agregando estados"
    This reverts commit 43c6e4d9ca7efbdbebd7e1ee3f4c58faabe8f702.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useState } from 'react/cjs/react.production.min';
 import App from './routes/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
-const Button = () => {
-    const [name, setName] = useState('Hola');
-  return (
-    <div>
-        <h1>{name}</h1>
-    </div>
-  )
-}
-
-export default Button 
