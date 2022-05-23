d109c3defbe08885ebba4a079b1e6884d4bdd524
Esteban Vasco <evasco.27@hotmail.com>
2022-05-23 08:55:38 -0500

    Formulario con useRef funcionando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '@styles/Login.scss'
-
 import yardSale from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
-    return (
+  const form = useRef(null);
 
+  const handleSummit  = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form.current)
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password')
+    }
+    console.log(data);
+  }
 
-
+  return (
 <div className="login">
     <div className="form-container">
       <img src={yardSale} alt="logo" className="logo"/>
 
-      <form action="/" className="form">
+      <form action="/" className="form" ref={form}>
         <label for="email" className="label">Email address</label>
-        <input type="text" id="email" placeholder="platzi@example.cm" className="input input-email"/>
+        <input type="text" name="email" placeholder="platzi@example.cm" className="input input-email"/>
 
         <label for="password" className="label">Password</label>
-        <input type="password" id="password" placeholder="*********" className="input input-password"/>
+        <input type="password" name="password" placeholder="*********" className="input input-password"/>
 
-        <input type="submit" value="Log in" className="primary-button login-button"/>
+        <button 
+        className="primary-button login-button"
+        onClick={handleSummit}
+        >Log in</button>
         <a href="/">Forgot my password</a>
       </form>
 
